7c7dd4eb1717b854b63a76ee57df4c1b93ca7e04
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-02-28 11:34:14 +0530

    created Fetch Chats

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,11 @@ import { Container } from "react-bootstrap";
 import NavBar from "./components/Navbar";
 import { use, useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    <>
+    <ChatContextProvider user = {user}>
       <NavBar />
       <Container>
         <Routes>
@@ -20,7 +21,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Container>
-    </>
+    </ChatContextProvider>
   );
 }
 
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,10 @@
+import { use, useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+
 const Chat = () => {
+
+    const {userChats, isUserChatsLoading, userChatsError} = useContext(ChatContext);
+    console.log("UserChats", userChats);
     return (  <>Chat App</> );
 }
  
